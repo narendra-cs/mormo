01f800296e19f187e27fb45fe105050cda7aa121
Pallav Jain <thepallav.jain@gmail.com>
2017-11-03 14:22:07 +0530

    Update and rename trap.sh to auto.sh

---
+++ auto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ol=0
+while true #run indefinitely
+do
+        inotifywait -r -e modify /var/log/com.log &&
+        echo $ol
+        ln=$(cat /var/log/com.log | wc -l)
+        nl=$((ln-ol))
+        awk "NR >=$ol && NR <=$ln" /var/log/com.log | awk '{ s = ""; for (i = 14; i <= NF; i++) s = s $i " "; print $10"\t"$3"\t"$8"\t"$6"\t"$12"\t"s}' > /var/log/mlog
+        ol=$ln
+	/root/Desktop/trap/logs_dump_to_mongo.py
+done
