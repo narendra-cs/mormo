f7b2ae1b76901cbb4befe01dc1c3b61d1ae5ab08
Pallav Jain <thepallav.jain@gmail.com>
2018-04-23 16:38:38 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -7,6 +7,6 @@ do
         ln=$(cat /var/log/com.log | wc -l)
         nl=$((ln-ol))
         awk "NR >=$ol && NR <=$ln" /var/log/com.log | awk '{ s = ""; for (i = 14; i <= NF; i++) s = s $i " "; print $10"\t"$3"\t"$8"\t"$6"\t"$12"\t"s}' > /var/log/mlog
-        ol=$ln
+        ol=$((ln+1))
 	/root/Desktop/trap/logs_dump_to_mongo.py
 done
