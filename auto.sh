f5973298b6eec96272080078eac5f887a142a7ef
narendra singh <narendra.cs.biet@gmail.com>
2018-04-30 16:08:39 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+wdir=${PWD}
 ol=0
-while true #run indefinitely
+
+while true #run infinitely
 do
-        inotifywait -r -e modify /var/log/com.log &&
-        echo $ol
-        ln=$(cat /var/log/com.log | wc -l)
-        nl=$((ln-ol))
-        awk "NR >=$ol && NR <=$ln" /var/log/com.log | awk '{ s = ""; for (i = 14; i <= NF; i++) s = s $i " "; print $10"\t"$3"\t"$8"\t"$6"\t"$12"\t"s}' > /var/log/mlog
-        ol=$ln
-	/root/Desktop/trap/logs_dump_to_mongo.py
+    inotifywait -r -e modify /var/log/com.log &&
+    echo $ol
+    ln=`cat /var/log/com.log | wc -l`
+    nl=$((ln-ol))
+    awk "NR >=$ol && NR <=$ln" /var/log/com.log | awk '{ s = ""; for (i = 14; i <= NF; i++) s = s $i " "; print $10"\t"$3"\t"$8"\t"$6"\t"$12"\t"s}' > /var/log/mlog
+    ol=$((ln+1))
+    ${wdir}/logs_dump_to_mongo.py
 done
