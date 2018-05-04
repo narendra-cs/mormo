98ddaf22add6f69f4dd3f7e846e19462086da17a
narendra singh <narendra.cs.biet@gmail.com>
2018-05-04 22:32:44 +0530

    Configuration parameter mechanism changed to YAML 'mormo.conf' file

--- config.py
+++ config.py
@@ -1,25 +1,39 @@
 # -*- coding: utf-8 -*-
+'''
+This file having function to load configuration from mormo.conf file i.e. default configuration file
+for the host system on which we have to capture the commands.
+'''
 
-def confLoader():
-    """ Load mormo configuration  """
+__author__ = "Narendra Singh"
 
-    # import yaml module to load configuration from yaml file
+
+def confLoader(filepath='mormo.conf'):
+    '''
+    :param filepath: configuration file name
+    :return: configuration as dictionary
+
+    Load mormo configuration and return it.
+    '''
+
+    # import yaml module to load configuration from yaml file with exception handling.
+    # so if module is not available it will print error.
     try:
+        '''
+        Import module thorugh __import__() so it would not be available as 
+        one of the function of this file when we import this file.
+        '''
         yaml    = __import__('yaml')
     except ImportError as e:
         print(e.__class__.__name__ + ": " + str(e))
-    
-    # name of yaml file
-    filepath    = 'mormo.conf'
 
-    # Loading mormo configuration from *.conf file
+    # Load mormo configuration from *.conf file. also handle excpetion if file is not there and in note proper YAML format.
     try:
         with open(filepath,"r") as fd0:
             data = yaml.load(fd0)
     except FileNotFoundError as e:
         print("Error {1}: {2} {0}".format(filepath,*e.args))
     except yaml.YAMLError as e:
-        print("Error:")
+        print("Error: configuration file is not in proper YAML format.")
         print(e)
     else:
         return data
--- db_functions.py
+++ db_functions.py
@@ -1,22 +1,43 @@
 # -*- coding: utf-8 -*-
+'''
+This file having utilit function Regarding, connection to databse i.e. MongoDB in our case.
+'''
+
+__author__ = "Narendra Singh"
+
 
 def mongoConnection():
-    """ MongoDB Connection to server and database define in mormo.conf """
-    
-    # import required modules
+    '''
+    :return: object of MongoClient
+
+    MongoDB Connection to server and database, define in mormo.conf .
+    '''
+
+    # Import required modules with exception handling. so if module is not available it will print error.
+    # else load configuration from mormo.conf file.
     try:
-        confload = __import__('config')
+        '''
+        Import module thorugh __import__() so it would not be available as 
+        one of the function of this file when we import this file.
+        '''
+        config = __import__('config')
         pymongo  = __import__('pymongo')
-        urllib   = __import__('urllib')
     except ImportError as e:
         print(e.__class__.__name__ + ": " + str(e))
+    else:
+        _cfg = config.confLoader()
 
-    # getting MongoDB conf from mormo.confg file
-    cnf  = confload.confLoader()
-    user = urllib.quote_plus(cnf['MONGODB']['USER'])
-    pwd  = urllib.quote_plus(cnf['MONGODB']['PASSWD'])
-    host = cnf['MONGODB']['HOST']
-    port = cnf['MONGODB']['PORT']
-    db   = cnf['MONGODB']['DBNAME']
-    uri  = 'mongodb://%s:%s@%s:%s/%s?authSource=%s' % (user,pwd,host,port,db,db)
-    return pymongo.MongoClient(uri).get_database(db)
+    # import "quote" function from urllib in python-2, if exception, import python-3 version of "quote"
+    try:
+        from urllib import quote
+    except ImportError:
+        from urllib.parse import quote
+
+    # Getting MongoDB conf and return MongoClient object to specified server and database.
+    if 'MONGODB' in _cfg.keys():
+        cnf  = _cfg['MONGODB']
+        uri  = 'mongodb://{user}:{pwd}@{host}:{port}/{db}?authSource={db}'.format(user=quote(cnf['USER']),pwd=quote(cnf['PASSWD']),host=cnf['HOST'],port=cnf['PORT'],db=cnf['DBNAME'])
+        return pymongo.MongoClient(uri).get_database(cnf['DBNAME'])
+    else:
+        print("mongodb configurations are not in mormo.conf file.")
+        return
